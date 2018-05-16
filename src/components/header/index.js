650f3a0036a25df2f0f6899ca16d3162d12cbba9
Matthias Adler <macedigital@users.noreply.github.com>
2018-05-16 16:13:39 +0200

    chore: Apply eslint rules
    Reformat all sourcefiles to adhere to eslint rules and use 2 spaces
    indentation consistently.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,17 +2,18 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class Header extends Component {
-	render() {
-		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link activeClassName={style.active} href="/">Home</Link>
-					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
-				</nav>
-			</header>
-		);
-	}
+  render() {
+    return (
+      <header class={style.header}>
+        <h1>Preact App</h1>
+        <nav>
+          <Link activeClassName={style.active} href="/">Home</Link>
+          <Link activeClassName={style.active} href="/profile">Me</Link>
+          <Link activeClassName={style.active} href="/profile/john">John</Link>
+        </nav>
+      </header>
+    );
+  }
 }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,14 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class Home extends Component {
-	render() {
-		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class={style.home}>
+        <h1>Home</h1>
+        <p>This is the Home component.</p>
+      </div>
+    );
+  }
 }
